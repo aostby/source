e7da0a63c67fb0a8b7ce8848434f785d3e0e0e40
aostby <aostby@gmail.com>
2025-12-31 17:39:18 +0100

    Fikk ikke til. LibVLC er for komprimerte formater, finn ut hvordan proprietær udp for UE2 kan løses

--- Kolibri.net.C64Sorter/Kolibri.net.C64Sorter/Forms/VideoStreamForm.cs
+++ Kolibri.net.C64Sorter/Kolibri.net.C64Sorter/Forms/VideoStreamForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Kolibri.net.C64Sorter.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LibVLCSharp.Shared;
+using LibVLCSharp.WinForms;
+
+
 
 namespace Kolibri.net.C64Sorter.Forms
 {
     public partial class VideoStreamForm : Form
-    {
+    {// Inside your Form class
+        private LibVLC _libVLC;
+        private MediaPlayer _mediaPlayer;
+        private UE2LogOn _hostname;
+
         public VideoStreamForm()
         {
             InitializeComponent();
+        }
+
+        public VideoStreamForm(UE2LogOn hostname)
+        {
+            InitializeComponent();
+            this._hostname = hostname;
+            Init();
+        }
+
+        private void Init()
+        {
+            try
+            {
+                Core.Initialize(); // Initialize LibVLC binaries
+
+                // _libVLC = new LibVLC(enableDebugLogs: true, options: new string[] { "--demux=mp4v", "--rawvid-fps=9" });
+                //  _libVLC = new LibVLC(enableDebugLogs: true, options: new string[] { "--demux=mpegts", "--rawvid-fps=9" });
+
+                _libVLC = new LibVLC(enableDebugLogs: true, options:new string[] { "--rawvid-fps=9" }); // Disable hardware decoding
+                _libVLC.Log += (sender, e) =>  SetStatusLabel($"[VLC] {e.Message}");
+
+                _mediaPlayer = new MediaPlayer(_libVLC);
+                videoview.MediaPlayer = _mediaPlayer;
+            }
+            catch (Exception ex)
+            {
+
+                SetStatusLabel($"Error {ex.GetType().Name} {ex.Message}");
+            }
+        }
+        public void SetStatusLabel(string statusText)
+        {
+            try { toolStripStatusLabelStatus.Text = statusText; }
+            catch (Exception) { }
+        }
+        private void startStreamToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Use '@' to listen on the specified port
+                Uri mrl = new Uri($"udp://{"192.168.5.65"}:11002");// $"udp://192.168.5.65:11000";// "udp://@:11000";
+                SetStatusLabel($"Starting media from {mrl}");
+
+                // Use :network-caching to give the player time to fill its buffer (in ms)
+                // Use :demux=h264 or :demux=mpegts if you know the specific format
+                string[] options = new[] { ":network-caching=300", ":clock-jitter=0" };
+                options = new string[] { "--demux=mpegts", "--rawvid-fps=9" };
+                options = new string[] { "--demux=mpegts", "--rawvid-fps=9" };
+
+                using (var media = new Media(_libVLC, mrl, options:options))
+                {
+                    _mediaPlayer.Play(media);
+                }
+            }
+            catch (Exception ex)
+            {  SetStatusLabel($"Error {ex.GetType().Name} {ex.Message}");
+            }
         }
     }
 }
